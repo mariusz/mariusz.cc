8db13f7580d06946e6949a8189ea095ad6045e89
Mariusz Ciesla <mariusz@dotmariusz.net>
2016-10-24 19:13:03 +0200

    HYPERSPACEEEEEEEEEEEEEEEE

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,34 +1,110 @@
 $(function() {
   var getDribbbleShots;
-  $.jribbble.setToken('2436518deac736cbcdf145c359fb78db1ea983e46701fbf01496df7476565866');
+  const { random, atan2, cos, sin, hypot } = Math;
+  const max = 200;
+  const canvas = document.querySelector('#teaserOverlay');
+  const $canvas = canvas.getContext('2d');
+  const body = document.body;
+  const particles = [];
 
-  class ColorSlider {
-    constructor(element, parameter, hue, saturation, lightness, alpha, tick) {
-      this._element = element;
-      this._parameter = parameter;
-      this._hue = hue;
-      this._saturation = saturation;
-      this._lightness = lightness;
-      this._tick = tick;
-      this._alpha = alpha;
-    }
+  let width = canvas.width = window.innerWidth;
+  let height = canvas.height = window.innerHeight;
+  let point = { x: width/2, y: height/2 };
+  let hue = 0;
 
-    recalculate() {
-      if (this._hue === 360) {
-        this._hue = 0;
+  function Particle(){};
+
+  Particle.prototype = {
+    init(){
+      this.hue = hue;
+      this.alpha = 0;
+      this.size = 1;
+      this.x = this.random(0, width);
+      this.y = this.random(0, height);
+      this.velocity = this.size * .5;
+      this.changed = null;
+      this.changedFrame = 0;
+      this.maxChangedFrames = 50;
+      return this;
+    },
+    draw(){
+      $canvas.fillStyle = `hsla(${this.hue}, 100%, 40%, ${this.alpha})`;
+      $canvas.beginPath();
+      $canvas.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      $canvas.fill();
+      this.update();
+    },
+    update(){
+      if(this.changed){
+        this.alpha *= .5;
+        this.changedFrame++;
+        if(this.changedFrame > this.maxChangedFrames){
+          this.reset();
+        }
+      } else if(this.distance(point.x, point.y) < 10){
+        this.changed = true;
       } else {
-        this._hue++;
-      }
-
-      this._element.style[this._parameter] = `hsla(${this._hue}, ${this._saturation}%, ${this._lightness}%, ${this._alpha})`;
-      window.requestAnimationFrame(() => this.draw());
-    }
-
-    draw() {
-      var frameRate = 1000 / this._tick;
-      window.setTimeout(() => this.recalculate(), frameRate);
+        let dx = point.x - this.x;
+        let dy = point.y - this.y;
+        let angle = atan2(dy, dx);
+        
+        this.alpha += .01;
+        this.x += this.velocity * cos(angle);
+        this.y += this.velocity * sin(angle);
+        this.velocity += .02;
+      } 
+    },
+    reset(){
+      this.init();
+    },
+    distance(x, y){
+      return hypot(x - this.x, y - this.y); 
+    },
+    random(min, max) {
+      return random() * (max - min) + min;
     }
   }
+
+  function animate(){
+    $canvas.fillStyle = `rgba(0,0,0, .2)`;
+    $canvas.fillRect(0, 0, width, height);
+    particles.forEach((p) => {
+      p.draw();
+    });
+    hue += .3;
+    window.requestAnimationFrame(animate);
+  }
+
+  function touches(e){
+    point.x = e.touches ? e.touches[0].clientX : e.clientX;
+    point.y = e.touches ? e.touches[0].clientY : e.clientY;
+  }
+
+  function setup(){
+    for(let i=0; i<max; i++){
+      setTimeout(() => {
+        let p = new Particle().init();
+        particles.push(p);
+      }, i * 10);
+    }
+    
+    canvas.addEventListener("mousemove", touches);
+    canvas.addEventListener("touchmove", touches);
+    canvas.addEventListener("mouseleave", () => {
+      point = { x: width/2, y: height/2 };
+    });
+    window.addEventListener("resize", () => {
+      width = canvas.width = window.innerWidth;
+      height = canvas.height = window.innerHeight;
+      point = { x: width/2, y: height/2 };
+    });
+    animate();
+  }
+
+  setup();
+
+
+  $.jribbble.setToken('2436518deac736cbcdf145c359fb78db1ea983e46701fbf01496df7476565866');
 
   getDribbbleShots = function() {
     return $.jribbble.users('mariusz').shots({
@@ -99,10 +175,6 @@ $(function() {
     var wpAbout = makeWP('about', 'index--about');
     var wpSkills = makeWP('skills', 'index--skills');
     var wpContact = makeWP('contact', 'index--contact');
-
-    var overlay = document.getElementById('teaserOverlay');
-    var slider = new ColorSlider(overlay, 'backgroundColor', 259, 27, 21, 0.5, 20);
-    slider.draw();
 
     return $('#testimonials_slider').flickity({
       contain: true,
